d4dd241460c0524846f3d9d3dd92fbedb02ca8da
杨柳 <yangliu@tiduyun.com>
2019-11-18 15:57:36 +0800

    添加信息

---
+++ config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-#
+# Name:         config
+# Description:  
+# Author:       10264
+# Date:         2019/11/18
+
+sleep_time_second = 1000
--- fire_fox.py
+++ fire_fox.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-#
-
-#-------------------------------------------------------------------------------
 # Name:         fire_fox
 # Description:  
 # Author:       10264
 # Date:         2019/11/18
-#-------------------------------------------------------------------------------
 from selenium import webdriver
+import time
+from config import *
+import urllib.request
+import requests
 
-browser = webdriver.Chrome()
-browser.get('https://www.iqiyi.com/v_19rvcvpfic.html')
+browser = None;
+
+def init():
+    """
+     浏览器信息初始化
+    :return:
+    """
+    browser = webdriver.Chrome()
+
+
+
+def open_html(url):
+    """
+     打开指定界面
+    """
+    browser.get(url)
+    time.sleep(sleep_time_second)
+
+
+
+
+
+if __name__ == '__main__':
+    init()
