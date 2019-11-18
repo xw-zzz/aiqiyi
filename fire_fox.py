64fa5fb1035cddcbab276361fd45ff8f6b85467f
杨柳 <yangliu@tiduyun.com>
2019-11-18 14:51:57 +0800

    Initial commit

---
+++ fire_fox.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-#
+
+#-------------------------------------------------------------------------------
+# Name:         fire_fox
+# Description:  
+# Author:       10264
+# Date:         2019/11/18
+#-------------------------------------------------------------------------------
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.iqiyi.com/v_19rvcvpfic.html')
